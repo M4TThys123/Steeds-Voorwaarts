cb67d643913be78cce23cd80efe7c55195458cf7
M4TThts123 <70703948+M4TThys123@users.noreply.github.com>
2023-09-05 17:33:32 +0200

    ✨ Working on features

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,6 @@
 <template>
   <HomeHero class="mb-5 "></HomeHero>
 
-  <NieuwsComponent02></NieuwsComponent02>
 
   <NieuwsComponent class="mb-5"></NieuwsComponent>
 
@@ -19,11 +18,10 @@ import NieuwsComponent from "@/lib/components/nieuws/NieuwsComponent.vue";
 import ContactSection from "@/lib/components/contact/ContactSection.vue";
 import HomeHero from "@/lib/components/home/HomeHero.vue";
 import RoosterComponent from "@/lib/components/rooster/RoosterComponent.vue";
-import NieuwsComponent02 from "@/lib/components/nieuws/NieuwsComponent02.vue";
 
 export default {
   name: "HomeView",
-  components: {NieuwsComponent02, RoosterComponent, HomeHero, ContactSection, NieuwsComponent, }
+  components: { RoosterComponent, HomeHero, ContactSection, NieuwsComponent, }
 }
 </script>
 
