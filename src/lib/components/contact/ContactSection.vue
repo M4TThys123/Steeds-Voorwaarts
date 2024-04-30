a5b442fe41ee6df7d5e515d81489807ba43d3cc9
M4TThts123 <70703948+M4TThys123@users.noreply.github.com>
2024-04-30 06:38:12 +0200

    ✨ Snelle fixes.

--- src/lib/components/contact/ContactSection.vue
+++ src/lib/components/contact/ContactSection.vue
@@ -1,123 +1,78 @@
 <template>
   <section class="contact-section container">
-    <div class="row ">
-      <div class="col-12 col-md-6 col-lg-8">
-        <div class="entry clr" itemprop="text">
-
+    <div class="row">
+      <div class="col-md-8">
+        <div>
+          <div>
+            <h2>Over ons</h2>
+            <p>Plezierig sporten in een ongedwongen sfeer onder gemotiveerde,
+              vakbekwame leiding staat bij ons centraal. Wil je kennismaken met onze vereniging, kom dan
+              gerust eens langs voor 2 gratis proeflessen.
+            </p>
+            <p>Wij bieden voor alle leeftijden volop sportieve mogelijkheden,
+              zoals: kleuterdans, hip hop en Freerunning voor jongens en meisjes, Total Body Workout en
+              fysiogym voor volwassenen en seniorengym voor ouderen.
+            </p>
+          </div>
 
           <div>
             <div>
+              <h3>Bestuur</h3>
               <div>
-                <div>
-                  <div>
-                    <h2>Over ons</h2>
-                    <p>Plezierig sporten in een ongedwongen sfeer onder gemotiveerde,
-                      vakbekwame leiding staat bij ons centraal. Wil je kennismaken met onze vereniging, kom dan
-                      gerust eens langs voor 2 gratis proeflessen.
-                    </p>
-                    <p>Wij bieden voor alle leeftijden volop sportieve mogelijkheden,
-                      zoals: kleuterdans, hip hop en Freerunning voor jongens en meisjes, Total Body Workout en
-                      fysiogym voor volwassenen en seniorengym voor ouderen.
-                    </p>
-                  </div>
-                </div>
+                <div>Voorzitter: Suzanne Dubbeldam</div>
+                <div>Penningmeester: Eva Poen-Visser</div>
+                <div>Secretaris: Jellie Bangma</div>
               </div>
             </div>
-
             <div>
-              <div>
-                <div>
-                  <h3>Bestuur</h3>
-                  <di>
-                    <div>Voorzitter: Suzanne Dubbeldam</div>
-                    <div>Penningmeester: Eva Poen-Visser</div>
-                    <div>Secretaris: Jellie Bangma</div>
-                    <div></div>
-                  </di>
-                </div>
-              </div>
-              <div id="panel-105-0-1-1" class="so-panel widget widget_sow-button panel-last-child" data-index="2">
-                <div class="so-widget-sow-button so-widget-sow-button-wire-727493327b27-105">
-                  <div class="ow-button-base ow-button-align-justify">
-                    <a href="https://steedsvoorwaarts.nl/wp-content/uploads/2021/11/Jaarverslag-2022_2023.pdf"
-                       class="ow-icon-placement-left ow-button-hover" target="_blank" rel="noopener noreferrer">
-		<span>
-			<span class="sow-icon-fontawesome sow-far" data-sow-icon="" style="" aria-hidden="true"></span>
-			Jaarverslag 2022-2023		</span>
-                    </a>
-                  </div>
-                </div>
-              </div>
+              <a href="https://steedsvoorwaarts.nl/wp-content/uploads/2021/11/Jaarverslag-2022_2023.pdf" target="_blank" class="btn btn-primary">
+                Jaarverslag 2022-2023
+              </a>
             </div>
           </div>
-          <div id="pg-105-1" class="panel-grid panel-no-style">
-            <div id="pgc-105-1-0" class="panel-grid-cell">
-              <div id="panel-105-1-0-0" class="so-panel widget widget_sow-editor panel-first-child panel-last-child"
-                   data-index="3">
-                <div class="so-widget-sow-editor so-widget-sow-editor-base">
-                  <div class="siteorigin-widget-tinymce textwidget">
-                    <p><a
-                        class="oajrlxb2 g5ia77u1 qu0x051f esr5mh6w e9989ue4 r7d6kgcz rq0escxv nhd2j8a9 nc684nl6 p7hjln8o kvgmc6g5 cxmmr5t8 oygrvhab hcukyx3x jb3vyjys rz4wbd8a qt6c0cv9 a8nywdso i1ao9s8h esuyzwwr f1sip0of lzcic4wl py34i1dx gpro0wi8"
-                        tabindex="0" role="link"
-                        href="https://www.google.com/maps/dir/?api=1&amp;destination=52.73151%2C5.02091&amp;fbclid=IwAR0WMEfqGF1d6mENVCbzGw-AUwnBzR_EDAb2ZZkTjqSS3-2HKMD9lz8wVPw"
-                        target="_blank" rel="nofollow noopener noreferrer">Burgemeester P. Kromplein 1 1657 AA
-                      Abbekerk</a></p>
-                    <div class="su-gmap su-u-responsive-media-yes">
-                      <iframe width="100%" height="400"
-                              src="//maps.google.com/maps?q=Burgemeester%20P.%20Kromplein%201%201657%20AA%20Abbekerk&amp;output=embed"
-                              title=""></iframe>
+        </div>
+        <div class="panel-grid-cell">
+          <div class="so-panel widget widget_sow-editor panel-first-child panel-last-child">
+            <div class="so-widget-sow-editor so-widget-sow-editor-base">
+              <div class="siteorigin-widget-tinymce textwidget">
+                <p>
+                  <a class="link" href="https://www.google.com/maps/dir/?api=1&amp;destination=52.73151%2C5.02091&amp;fbclid=IwAR0WMEfqGF1d6mENVCbzGw-AUwnBzR_EDAb2ZZkTjqSS3-2HKMD9lz8wVPw" target="_blank" rel="nofollow noopener noreferrer">
+                    Burgemeester P. Kromplein 1 1657 AA Abbekerk
+                  </a>
+                </p>
+                <div id="panel-105-1-2-0" class="so-panel widget widget_sow-editor panel-first-child" data-index="5">
+                  <div class="so-widget-sow-editor so-widget-sow-editor-base"><h3 class="widget-title">Contact</h3>
+                    <div class="siteorigin-widget-tinymce textwidget">
+                      <p style="text-align: left;">Postadres:<br>
+                        Zuideinde 4<br>
+                        1657 ED Abbekerk</p>
+                      <p style="text-align: left;">E-mail: <a href="mailto:svwabbekerk1913@gmail.com">svwabbekerk1913@gmail.com</a>
+                      </p>
+                      <p style="text-align: left;">Bankrekening: NL36 INGB 0004638688</p>
+                      <p style="text-align: left;">Facebook: <a href="http://www.facebook.com/steedsvoorwaartsAbbekerk">http://www.facebook.com/steedsvoorwaartsAbbekerk</a>
+                      </p>
                     </div>
                   </div>
                 </div>
-              </div>
-            </div>
-            <div id="pgc-105-1-1" class="panel-grid-cell">
-              <div id="panel-105-1-1-0"
-                   class="so-panel widget widget_ocean_facebook widget_facebook_likebox panel-first-child panel-last-child"
-                   data-index="4">
-                <iframe
-                    src="https://www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Fwww.facebook.com%2FsteedsvoorwaartsAbbekerk&amp;width=500&amp;height=300&amp;colorscheme=light&amp;show_faces=true&amp;stream=true&amp;show_border=true&amp;header=false&amp;force_wall=false"
-                    scrolling="no" frameborder="0"
-                    style="border: none; overflow: hidden; width: 500px;  height: 300px; background: #fff"></iframe>
-              </div>
-            </div>
-            <div id="pgc-105-1-2" class="panel-grid-cell">
-              <div id="panel-105-1-2-0" class="so-panel widget widget_sow-editor panel-first-child" data-index="5">
-                <div class="so-widget-sow-editor so-widget-sow-editor-base"><h3 class="widget-title">Contact</h3>
-                  <div class="siteorigin-widget-tinymce textwidget">
-                    <p style="text-align: left;">Postadres:<br>
-                      Zuideinde 4<br>
-                      1657 ED Abbekerk</p>
-                    <p style="text-align: left;">E-mail: <a href="mailto:svwabbekerk1913@gmail.com">svwabbekerk1913@gmail.com</a>
-                    </p>
-                    <p style="text-align: left;">Bankrekening: NL36 INGB 0004638688</p>
-                    <p style="text-align: left;">Facebook: <a href="http://www.facebook.com/steedsvoorwaartsAbbekerk">http://www.facebook.com/steedsvoorwaartsAbbekerk</a>
-                    </p>
-                  </div>
-                </div>
-              </div>
-              <div id="panel-105-1-2-1" class="so-panel widget widget_sow-button panel-last-child" data-index="6">
-                <div class="so-widget-sow-button so-widget-sow-button-wire-727493327b27-105">
-                  <div class="ow-button-base ow-button-align-justify">
-                    <a href="https://steedsvoorwaarts.nl/wp-content/uploads/2021/08/Inschrijfformulier-2021-2022.pdf"
-                       class="ow-icon-placement-left ow-button-hover" target="_blank" rel="noopener noreferrer">
-		<span>
-			<span class="sow-icon-fontawesome sow-far" data-sow-icon="" style="" aria-hidden="true"></span>
-			Inschrijfformulier		</span>
-                    </a>
-                  </div>
+
+                <div class="su-gmap su-u-responsive-media-yes">
+                  <iframe width="100%" height="400" src="//maps.google.com/maps?q=Burgemeester%20P.%20Kromplein%201%201657%20AA%20Abbekerk&amp;output=embed" title=""></iframe>
                 </div>
               </div>
             </div>
           </div>
         </div>
-
+        <div class="panel-grid-cell">
+          <div class="so-panel widget widget_ocean_facebook widget_facebook_likebox panel-first-child panel-last-child">
+            <iframe src="https://www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Fwww.facebook.com%2FsteedsvoorwaartsAbbekerk&amp;width=500&amp;height=300&amp;colorscheme=light&amp;show_faces=true&amp;stream=true&amp;show_border=true&amp;header=false&amp;force_wall=false" scrolling="no" frameborder="0" style="border: none; overflow: hidden; width: 100%; height: 300px; background: #fff"></iframe>
+          </div>
+        </div>
       </div>
-    </div>
 
-    <div class="col-12 col-md-6 col-lg-4">
-      <ContactInformation class="mb-5"></ContactInformation>
-      <ContactForm class="my-5"></ContactForm>
+      <div class="col-md-4">
+        <ContactInformation class="mb-5"></ContactInformation>
+        <ContactForm class="my-5"></ContactForm>
+      </div>
     </div>
   </section>
 </template>
@@ -128,10 +83,12 @@ import ContactInformation from "@/lib/components/contact/ContactInformation.vue"
 
 export default {
   name: "ContactSection",
-  components: {ContactInformation, ContactForm}
+  components: { ContactInformation, ContactForm }
 }
 </script>
 
 <style scoped>
 
 </style>
+
+
