1e7e42fc8ade98cc0250cb12b5b811fdb7e44ba4
Matthijs Blauw <70703948+M4TThys123@users.noreply.github.com>
2024-09-06 16:54:41 +0200

    ✨ Made changes to the ContactComponent.

--- src/lib/components/contact/ContactInformation.vue
+++ src/lib/components/contact/ContactInformation.vue
@@ -3,19 +3,33 @@
     <h2 class="mb-4">Steeds Voorwaarts</h2>
     <section class="contact-info__item row">
       <strong class="col-4 col-sm-3">Telefoon:</strong>
-      <p class="col-6">{{ telephone }}</p>
+      <a class="col-6" @click.prevent="callPhone(telephone)" href="#">
+        {{ telephone }}
+      </a>
     </section>
     <section class="contact-info__item row">
       <strong class="col-4 col-sm-3">Email:</strong>
-      <p class="col-6">{{ email }}</p>
+      <a class="col-6" @click.prevent="sendEmail(email)" href="#">
+        {{ email }}
+      </a>
     </section>
     <section class="contact-info__item row">
       <strong class="col-4 col-sm-3 col-xs-1">Adres:</strong>
-      <p class="col-6">{{ address }} <br> <br>{{ city }}</p>
+      <p class="col-6" @click="openInGoogleMaps(address)">
+        {{ address.street }} <br>
+        {{ address.postalcode }} {{ address.city }}
+      </p>
     </section>
     <section class="contact-info__item row">
-      <strong class="col-4 col-sm-3">KVK:</strong>
-      <p class="col-6">{{ kvkNumber }}</p>
+      <strong class="col-4 col-sm-3 col-xs-1">Postadres:</strong>
+      <p class="col-6" @click="openInGoogleMaps(postaddress)">
+        {{ postaddress.street }} <br>
+        {{ postaddress.postalcode }} {{ postaddress.city }}
+      </p>
+    </section>
+    <section class="contact-info__item row">
+      <strong class="col-4 col-sm-3">Iban:</strong>
+      <p @click="copyIban" class="col-6">{{ iban }}</p>
     </section>
   </section>
 </template>
@@ -25,13 +39,38 @@ export default {
   name: "ContactInformation",
   data() {
     return {
-      address: "Burgemeester P. Kromplein 1,",
-      city: "1657 AA Abbekerk",
+      address: {
+        street: "Burgemeester P. Kromplein 1",
+        postalcode: "1657 AA",
+        city: "Abbekerk"
+      }, 
+      postaddress: {
+        street: "Zuideinde 4",
+        postalcode: "1657 ED",
+        city: "Abbekerk"
+      }, 
       telephone: "06 12345678",
       email: "svwabbekerk1913@gmail.com",
-      kvkNumber: "12345678"
+      iban: " NL36INGB0004638688"
     };
-  }
+  },
+  methods: {
+    openInGoogleMaps(address) {
+      const query = `${address.street}, ${address.postalcode} ${address.city}`;
+      const url = `https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(query)}`;
+      window.open(url, '_blank');
+    },
+    copyIban() {
+      navigator.clipboard.writeText(this.iban).then(() => {
+      })
+    },
+    callPhone(phoneNumber) {
+      window.location.href = `tel:${phoneNumber}`;
+    },
+    sendEmail(emailAddress) {
+      window.location.href = `mailto:${emailAddress}`;
+    },
+  },
 };
 </script>
 
@@ -48,7 +87,14 @@ export default {
 .contact-info__item {
   display: flex;
 }
+
+.contact-info__item > a{
+  text-decoration: underline var(--primary-button) 2px !important;
+}
 .contact-info__item > p{
   text-decoration: underline var(--primary-button) 2px;
+}
+p.col-6 {
+  cursor: pointer; /* Change cursor to pointer to indicate it's clickable */
 }
 </style>
--- src/lib/components/contact/ContactSection.vue
+++ src/lib/components/contact/ContactSection.vue
@@ -3,80 +3,77 @@
     <div class="row">
       <div class="col-md-8">
         <div>
-          <div>
-            <h2>Over ons</h2>
-            <p>Plezierig sporten in een ongedwongen sfeer onder gemotiveerde,
-              vakbekwame leiding staat bij ons centraal. Wil je kennismaken met onze vereniging, kom dan
-              gerust eens langs voor 2 gratis proeflessen.
-            </p>
-            <p>Wij bieden voor alle leeftijden volop sportieve mogelijkheden,
-              zoals: <b>Kleuterdans</b>, <b>Hip Hop</b> en <b>Freerunning</b> voor jongens en meisjes, <b>Total Body Workout</b> en
-              <b>Fysiogym</b> voor volwassenen en <b>Seniorengym</b> voor ouderen.
-            </p>
-          </div>
+          <h2>Over ons</h2>
+          <p>
+            Plezierig sporten in een ongedwongen sfeer onder gemotiveerde,
+            vakbekwame leiding staat bij ons centraal. Wil je kennismaken met
+            onze vereniging, kom dan gerust eens langs voor 2 gratis proeflessen.
+          </p>
+          <p>
+            Wij bieden voor alle leeftijden volop sportieve mogelijkheden, zoals: 
+            <b>Kleuterdans</b>, <b>Hip Hop</b> en <b>Freerunning</b> voor jongens en meisjes, 
+            <b>Total Body Workout</b> en <b>Fysiogym</b> voor volwassenen en 
+            <b>Seniorengym</b> voor ouderen.
+          </p>
+        </div>
 
+        <div>
+          <h3>Bestuur</h3>
           <div>
-            <div>
-              <h3>Bestuur</h3>
-              <div>
-                <div>Voorzitter: <b>Suzanne Dubbeldam</b></div>
-                <div>Penningmeester: <b>Femke van der Linden</b></div>
-                <div>Secretaris: <b>Jellie Bangma</b></div>
-              </div>
-            </div>
-
+            <p>Voorzitter: <b>Suzanne Dubbeldam</b></p>
+            <p>Penningmeester: <b>Femke van der Linden</b></p>
+            <p>Secretaris: <b>Jellie Bangma</b></p>
           </div>
         </div>
-        <div class="panel-grid-cell">
-          <div class="so-panel widget widget_sow-editor panel-first-child panel-last-child">
-            <div class="so-widget-sow-editor so-widget-sow-editor-base">
-              <div class="siteorigin-widget-tinymce textwidget">
-                <p>
-                  <a class="link" href="https://www.google.com/maps/dir/?api=1&amp;destination=52.73151%2C5.02091&amp;fbclid=IwAR0WMEfqGF1d6mENVCbzGw-AUwnBzR_EDAb2ZZkTjqSS3-2HKMD9lz8wVPw" target="_blank" rel="nofollow noopener noreferrer">
-                    Burgemeester P. Kromplein 1 1657 AA Abbekerk
-                  </a>
-                </p>
 
-                <div>
-                  <genaric-buton class="my-5"
-                                  icon="bx bxs-file-pdf"
-                                 text="Jaarverslag 2022-2023"
-                                 link="https://steedsvoorwaarts.nl/wp-content/uploads/2021/11/Jaarverslag-2022_2023.pdf">
+        <div class="my-5">
+          <p>
+            <a class="link" href="https://www.google.com/maps/dir/?api=1&amp;destination=52.73151%2C5.02091" target="_blank" rel="nofollow noopener noreferrer">
+              Burgemeester P. Kromplein 1 1657 AA Abbekerk
+            </a>
+          </p>
 
-                  </genaric-buton>
-                </div>
+          <div>
+            <GenaricButon
+              class="mt-4"
+              icon="bx bxs-file-pdf"
+              text="Jaarverslag 2022-2023"
+              link="https://steedsvoorwaarts.nl/wp-content/uploads/2021/11/Jaarverslag-2022_2023.pdf"
+            />
+          </div>
 
-                <div id="panel-105-1-2-0" class="so-panel widget widget_sow-editor panel-first-child" data-index="5">
-                  <div class="so-widget-sow-editor so-widget-sow-editor-base"><h3 class="widget-title">Contact</h3>
-                    <div class="siteorigin-widget-tinymce textwidget">
-                      <p style="text-align: left;">Postadres:<br>
-                        Zuideinde 4<br>
-                        1657 ED Abbekerk</p>
-                      <p style="text-align: left;">E-mail: <a href="mailto:svwabbekerk1913@gmail.com">svwabbekerk1913@gmail.com</a>
-                      </p>
-                      <p style="text-align: left;">Bankrekening: NL36 INGB 0004638688</p>
-                      <p style="text-align: left;">Facebook: <a href="http://www.facebook.com/steedsvoorwaartsAbbekerk">http://www.facebook.com/steedsvoorwaartsAbbekerk</a>
-                      </p>
-                    </div>
-                  </div>
-                </div>
+          <h3 class="mt-4">Contact</h3>
+          <div>
+            <p class="text-left">
+              Postadres:<br />
+              Zuideinde 4<br />
+              1657 ED Abbekerk
+            </p>
+            <p class="text-left">
+              E-mail: 
+              <a href="mailto:svwabbekerk1913@gmail.com">svwabbekerk1913@gmail.com</a>
+            </p>
+            <p class="text-left">Bankrekening: NL36 INGB 0004638688</p>
+            <p class="text-left">
+              Facebook: 
+              <a href="http://www.facebook.com/steedsvoorwaartsAbbekerk">http://www.facebook.com/steedsvoorwaartsAbbekerk</a>
+            </p>
+          </div>
 
-                <div class="su-gmap su-u-responsive-media-yes">
-                  <iframe width="100%" height="400" src="//maps.google.com/maps?q=Burgemeester%20P.%20Kromplein%201%201657%20AA%20Abbekerk&amp;output=embed" title=""></iframe>
-                </div>
-              </div>
-            </div>
+          <div class="my-4">
+            <iframe
+              width="100%"
+              height="400"
+              src="//maps.google.com/maps?q=Burgemeester%20P.%20Kromplein%201%201657%20AA%20Abbekerk&amp;output=embed"
+              title="Google Maps"
+            ></iframe>
           </div>
         </div>
       </div>
 
       <div class="col-md-4">
-
-
         <ContactInformation class="mb-5"></ContactInformation>
         <ContactForm class="my-5"></ContactForm>
-
-<!--        <FacebookComponent></FacebookComponent>-->
       </div>
     </div>
   </section>
@@ -89,12 +86,8 @@ import GenaricButon from "@/lib/components/elements/GenaricButon.vue";
 
 export default {
   name: "ContactSection",
-  components: {GenaricButon, ContactInformation, ContactForm }
-}
+  components: { GenaricButon, ContactInformation, ContactForm },
+};
 </script>
 
-<style scoped>
-
-</style>
-
-
+<style scoped></style>
