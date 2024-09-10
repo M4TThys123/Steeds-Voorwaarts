f3769091bac5f9c8b0b606285a9d9c0ab731d458
Matthijs Blauw <70703948+M4TThys123@users.noreply.github.com>
2024-09-10 16:29:32 +0200

    ⬆️ Made a working version of Vuetify.

--- src/main.js
+++ src/main.js
@@ -5,7 +5,18 @@ import router from '../src/router/router'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.js'
 
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
+const vuetify = createVuetify({
+    components,
+    directives,
+  })
+  
 const app = createApp(App)
 app.use(router)
+app.use(vuetify)
 app.mount('#app')
